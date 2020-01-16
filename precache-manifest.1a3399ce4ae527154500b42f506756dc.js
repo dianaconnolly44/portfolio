1265dddb81a9288934edce25467a473a549912aa
Diana Connolly <dianaconnolly44@gmail.com>
2020-01-16 11:01:59 -0500

    Updates

---
+++ precache-manifest.1a3399ce4ae527154500b42f506756dc.js
@@ -0,0 +1,466 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ae21412c64dcb8e6559e491e3a7187e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "500c7846dbbece0ae42e",
+    "url": "/static/css/main.75a69e0a.chunk.css"
+  },
+  {
+    "revision": "ceb234ac0a90e56f1be9",
+    "url": "/static/js/2.61250a5c.chunk.js"
+  },
+  {
+    "revision": "584c45417e82645276fc6d4400fbae22",
+    "url": "/static/js/2.61250a5c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "500c7846dbbece0ae42e",
+    "url": "/static/js/main.7c6df828.chunk.js"
+  },
+  {
+    "revision": "00ccf1beb4d435cfbe60",
+    "url": "/static/js/runtime-main.c7cc6e23.js"
+  },
+  {
+    "revision": "aa9093f293c97c85c009a95079b8e20c",
+    "url": "/static/media/104bodies.aa9093f2.gif"
+  },
+  {
+    "revision": "dbe617a91bf8bc2cd902162d499b52f1",
+    "url": "/static/media/104eyes.dbe617a9.gif"
+  },
+  {
+    "revision": "cfcb8a4a12851182314e2b8b35f01afc",
+    "url": "/static/media/104eyes_.cfcb8a4a.gif"
+  },
+  {
+    "revision": "5f985fe478ebe78b01e01e8dbaf814a7",
+    "url": "/static/media/104faces.5f985fe4.gif"
+  },
+  {
+    "revision": "5d9df0ccfc8cf839ef860b35d070c645",
+    "url": "/static/media/104oppositeBalls.5d9df0cc.gif"
+  },
+  {
+    "revision": "7ff33f68fc0dfb7541716c8117d67bac",
+    "url": "/static/media/104paddleBall.7ff33f68.gif"
+  },
+  {
+    "revision": "ca561664f40bab19ebe133f682d3b5a9",
+    "url": "/static/media/104walkCycle.ca561664.gif"
+  },
+  {
+    "revision": "90fd1ea87ffbe19486af582100061de5",
+    "url": "/static/media/Lint_s.90fd1ea8.jpg"
+  },
+  {
+    "revision": "939525afb421cc43b31b05cb5ebb9b69",
+    "url": "/static/media/NYE-small.939525af.gif"
+  },
+  {
+    "revision": "2cb0f9458d6eb61f91192b28cd698b86",
+    "url": "/static/media/achieve.2cb0f945.jpg"
+  },
+  {
+    "revision": "5f39ed85253b6f0ef4ce2b73f2924661",
+    "url": "/static/media/activity.5f39ed85.jpg"
+  },
+  {
+    "revision": "1270c31f484f2821707363bfc7a7ed0c",
+    "url": "/static/media/activity2.1270c31f.jpg"
+  },
+  {
+    "revision": "c3caea504e04ab3318795eb854e4718b",
+    "url": "/static/media/activity3.c3caea50.jpg"
+  },
+  {
+    "revision": "540cea3748b1232ffc5294262c492d9d",
+    "url": "/static/media/alyssa.540cea37.jpg"
+  },
+  {
+    "revision": "e02f02b9c1c090bc1ec75bc5d3b50872",
+    "url": "/static/media/artBioPetri.e02f02b9.jpg"
+  },
+  {
+    "revision": "05edd6a45b9edc874db3549a7b3f492c",
+    "url": "/static/media/assetCreation.05edd6a4.png"
+  },
+  {
+    "revision": "5feb1980681ad65d4a3d5ac217950f8b",
+    "url": "/static/media/bFriday-small.5feb1980.gif"
+  },
+  {
+    "revision": "6823d456b03a438bf18435db1b199c49",
+    "url": "/static/media/banana.6823d456.gif"
+  },
+  {
+    "revision": "1fd7b38dc6a2c5ae979f4897460ab967",
+    "url": "/static/media/boobs_s.1fd7b38d.jpg"
+  },
+  {
+    "revision": "5f2167a57ae3afbc4c7e8166fa360b71",
+    "url": "/static/media/botanical.5f2167a5.jpg"
+  },
+  {
+    "revision": "bcb23fe88c891e6c50138d42f3648a1f",
+    "url": "/static/media/botanical_Ginkgo.bcb23fe8.jpg"
+  },
+  {
+    "revision": "0bc348105671ab8d08e98908b2abbaa8",
+    "url": "/static/media/botanical_Poppy.0bc34810.jpg"
+  },
+  {
+    "revision": "6572bbc8c297fb9b00c921f4a44ac3e2",
+    "url": "/static/media/botanical_Sequoia.6572bbc8.jpg"
+  },
+  {
+    "revision": "3a4fca470a3357ba2bac92c969fc492a",
+    "url": "/static/media/botanical_SnakePlant.3a4fca47.jpg"
+  },
+  {
+    "revision": "91c87fa7607d22a6b9d314436c2f269b",
+    "url": "/static/media/bunny.91c87fa7.jpg"
+  },
+  {
+    "revision": "8b3b9a85ddc80b8ea84cf0bd3b24bcf5",
+    "url": "/static/media/characterExplainer.8b3b9a85.gif"
+  },
+  {
+    "revision": "937839ea9166d37779e08c94288c5a52",
+    "url": "/static/media/charcoal.937839ea.jpg"
+  },
+  {
+    "revision": "f78dbfc1663b939a299085ad4400db77",
+    "url": "/static/media/clock-small.f78dbfc1.gif"
+  },
+  {
+    "revision": "a3e02f79fa916866027876bee67f8e18",
+    "url": "/static/media/color.a3e02f79.jpg"
+  },
+  {
+    "revision": "481f71290bc96ae2a64fba9ea583bb42",
+    "url": "/static/media/colorShadowGirl.481f7129.jpg"
+  },
+  {
+    "revision": "232587f03b93b2ac9c4972e80d9c000c",
+    "url": "/static/media/dadMotorcycle.232587f0.gif"
+  },
+  {
+    "revision": "2fe89e8b8fa5b18639d32637c4ebdfae",
+    "url": "/static/media/dadMotorcyclePurple.2fe89e8b.gif"
+  },
+  {
+    "revision": "d261e655e473a08eb193054c697f4a0d",
+    "url": "/static/media/dadcard.d261e655.gif"
+  },
+  {
+    "revision": "2b3739eae8b50775481e1179511bdcbc",
+    "url": "/static/media/dissect_s.2b3739ea.jpg"
+  },
+  {
+    "revision": "0bfdbf8531ea3c31649d7f65fa08ac4b",
+    "url": "/static/media/dreamBox.0bfdbf85.jpg"
+  },
+  {
+    "revision": "e80f2a88037991ad3df59b009c5a8d61",
+    "url": "/static/media/dreamBox.e80f2a88.gif"
+  },
+  {
+    "revision": "a1fbb26ac54e308b79f3a56bee5f97ba",
+    "url": "/static/media/dreamBox_.a1fbb26a.gif"
+  },
+  {
+    "revision": "a655b19c6e1339b0093e1c8d4e53eff8",
+    "url": "/static/media/ducks.a655b19c.jpg"
+  },
+  {
+    "revision": "22ca4cc433cd11b69c63eddac5c4e5ec",
+    "url": "/static/media/energy.22ca4cc4.jpg"
+  },
+  {
+    "revision": "91362c1c9932d1c0dd2312ea03c5ee7c",
+    "url": "/static/media/expii.91362c1c.png"
+  },
+  {
+    "revision": "d323bde4306cc705adb9178f5e444abc",
+    "url": "/static/media/eye_s.d323bde4.jpg"
+  },
+  {
+    "revision": "2f5df3b37813d59295cbf80f5c8dfb10",
+    "url": "/static/media/fall.2f5df3b3.jpg"
+  },
+  {
+    "revision": "2a4ad6a3538e7c16784c8787be25d554",
+    "url": "/static/media/fall2.2a4ad6a3.jpg"
+  },
+  {
+    "revision": "27f9153db5f945e5afeb57bc1f577d25",
+    "url": "/static/media/figure1.27f9153d.jpg"
+  },
+  {
+    "revision": "9aefaa1844382fe78d3bf131b9f11157",
+    "url": "/static/media/figure2.9aefaa18.jpg"
+  },
+  {
+    "revision": "8c350730d74e922d9dcfbc5e1c8ac593",
+    "url": "/static/media/figure3.8c350730.jpg"
+  },
+  {
+    "revision": "67ad90acf979cf49e5a6cf800e32beab",
+    "url": "/static/media/figure4.67ad90ac.jpg"
+  },
+  {
+    "revision": "259d4d501a7ac644c32a18f63b01c3c6",
+    "url": "/static/media/figure5.259d4d50.jpg"
+  },
+  {
+    "revision": "6fd2e8f202d6b9e404b0c05532b79525",
+    "url": "/static/media/flipfoot.6fd2e8f2.jpg"
+  },
+  {
+    "revision": "619bdb37332605f514bccdd447d5766c",
+    "url": "/static/media/frameByFrame.619bdb37.gif"
+  },
+  {
+    "revision": "59ccfab163cd796769b19ef970ed97b7",
+    "url": "/static/media/frida.59ccfab1.jpg"
+  },
+  {
+    "revision": "d76b7c8cb175349e74612f36b4e91937",
+    "url": "/static/media/gaucher.d76b7c8c.jpg"
+  },
+  {
+    "revision": "ee742f034473a39d89c82a6313335c15",
+    "url": "/static/media/gaucher2.ee742f03.jpg"
+  },
+  {
+    "revision": "bcbc2797eaf31f71305a3083d9415687",
+    "url": "/static/media/hairGod.bcbc2797.jpg"
+  },
+  {
+    "revision": "70f6c7e2e8fdf30b49b2449f12f6bb6d",
+    "url": "/static/media/handCharcoal.70f6c7e2.jpg"
+  },
+  {
+    "revision": "e5f34c626b0018bb25838d458ebc0190",
+    "url": "/static/media/hanukkah-small.e5f34c62.gif"
+  },
+  {
+    "revision": "721100971a92d968180e2607895dec87",
+    "url": "/static/media/heart.72110097.gif"
+  },
+  {
+    "revision": "7b96fcc16c80cb230ca82b87a11086d3",
+    "url": "/static/media/holiday.7b96fcc1.gif"
+  },
+  {
+    "revision": "6850c38fc5daa74a8f3075ee120be57c",
+    "url": "/static/media/huesGrid_s.6850c38f.jpg"
+  },
+  {
+    "revision": "2f3f491447fc21b490c8eb515db36ed7",
+    "url": "/static/media/huesGrid_sB&W.2f3f4914.jpg"
+  },
+  {
+    "revision": "064943e13ec0ad53b9285be19137dd20",
+    "url": "/static/media/iconExplainer.064943e1.gif"
+  },
+  {
+    "revision": "91debc83b4e5c998f78e9ea7999f5ce7",
+    "url": "/static/media/icyGirl.91debc83.jpg"
+  },
+  {
+    "revision": "19547efe84b7b9606052178d0c512e74",
+    "url": "/static/media/inkHair.19547efe.jpg"
+  },
+  {
+    "revision": "6ce2aa5b1c6a79dd122f4ad7a9b610c6",
+    "url": "/static/media/jackson.6ce2aa5b.jpg"
+  },
+  {
+    "revision": "ef1bb56c75082c3fcd726df232eb3aaa",
+    "url": "/static/media/jesusme_s.ef1bb56c.jpg"
+  },
+  {
+    "revision": "d99f1cecda4368961bba23168d37caed",
+    "url": "/static/media/joker.d99f1cec.jpg"
+  },
+  {
+    "revision": "f8f5e570ecac1c21bdd5ec518d376507",
+    "url": "/static/media/lloyd.f8f5e570.jpg"
+  },
+  {
+    "revision": "497527ab727bef94780cc10da8cc89b7",
+    "url": "/static/media/migraine.497527ab.jpg"
+  },
+  {
+    "revision": "8997e8c11ea099f5c173153ea2e1677a",
+    "url": "/static/media/model_s.8997e8c1.jpg"
+  },
+  {
+    "revision": "a46aea07da09802f2c0f31ece1a69faa",
+    "url": "/static/media/orangeDrawing.a46aea07.jpg"
+  },
+  {
+    "revision": "d20b7c373fe2199d277672f574be9d55",
+    "url": "/static/media/ornaments-small.d20b7c37.gif"
+  },
+  {
+    "revision": "b85b875549ed2fbf4338bdbdda316ca6",
+    "url": "/static/media/pathways.b85b8755.jpg"
+  },
+  {
+    "revision": "90a4cdc274cb6f4e647c1534c326914c",
+    "url": "/static/media/phenotype.90a4cdc2.jpg"
+  },
+  {
+    "revision": "106469ee79c0e3fdbb7381b666e2d8cd",
+    "url": "/static/media/plane_s.106469ee.jpg"
+  },
+  {
+    "revision": "25154e34bf8c33e3db623dde2c6bb697",
+    "url": "/static/media/pumpkins-small.25154e34.gif"
+  },
+  {
+    "revision": "fc97d31cf57846dcba2507cab0c9a9af",
+    "url": "/static/media/punkNecklace.fc97d31c.jpg"
+  },
+  {
+    "revision": "f9822efb48f80df0aca42b106a022035",
+    "url": "/static/media/ribbon-small.f9822efb.gif"
+  },
+  {
+    "revision": "6d4823dcfd601fb5655d729206e753f3",
+    "url": "/static/media/rococoLint_s.6d4823dc.jpg"
+  },
+  {
+    "revision": "4e32034622a818a7aa50d3c80bfa3810",
+    "url": "/static/media/selfPencil.4e320346.jpg"
+  },
+  {
+    "revision": "c1047846efc7d668ff6156465a3537aa",
+    "url": "/static/media/shell.c1047846.jpg"
+  },
+  {
+    "revision": "7bd344b6d9a7b208ec510b06db2d46b4",
+    "url": "/static/media/skulls-small.7bd344b6.gif"
+  },
+  {
+    "revision": "bb255aa95a9a2a887c86e4ee57bdff65",
+    "url": "/static/media/spaceArt.bb255aa9.jpg"
+  },
+  {
+    "revision": "6ae9b391f742afb0d9cdf6e2a2c75e33",
+    "url": "/static/media/stillLifePencil.6ae9b391.jpg"
+  },
+  {
+    "revision": "bd3b79150e8496da47cbd4ee6ff1c4e7",
+    "url": "/static/media/t1d.bd3b7915.jpg"
+  },
+  {
+    "revision": "fbeada4f7013951131ca24b0b14511a7",
+    "url": "/static/media/t1d2.fbeada4f.jpg"
+  },
+  {
+    "revision": "cd17ebd922f12cb89cb55ae7ab04d2a5",
+    "url": "/static/media/t1d3.cd17ebd9.jpg"
+  },
+  {
+    "revision": "6e2619eceddd6f33e9e8660950140474",
+    "url": "/static/media/textAnimation.6e2619ec.gif"
+  },
+  {
+    "revision": "be93127083445cebf04632bab85250dc",
+    "url": "/static/media/thesis.be931270.png"
+  },
+  {
+    "revision": "d5ce87c2d71cc6982b7b714c5fb5efef",
+    "url": "/static/media/thesis1.d5ce87c2.jpg"
+  },
+  {
+    "revision": "d35b9e13510aa0e21dc766339fd2f4f4",
+    "url": "/static/media/thesis2.d35b9e13.jpg"
+  },
+  {
+    "revision": "98d958afb431f77e22dff0e9ad43efb5",
+    "url": "/static/media/thesis3.98d958af.jpg"
+  },
+  {
+    "revision": "c1bd4b2e8b748ee1de53180ae588f5c9",
+    "url": "/static/media/trump.c1bd4b2e.jpg"
+  },
+  {
+    "revision": "d542d16bd53adf0366cd7d4a89d45f20",
+    "url": "/static/media/turkey-small.d542d16b.gif"
+  },
+  {
+    "revision": "a894a24ed2b85ec9b42af58ef23e28ae",
+    "url": "/static/media/verdeBaby.a894a24e.jpg"
+  },
+  {
+    "revision": "50fd1ff0c64edf4174349cc9f05ff004",
+    "url": "/static/media/verdeElephant.50fd1ff0.jpg"
+  },
+  {
+    "revision": "5de7af0060cd5d63b09c81c53d675e0a",
+    "url": "/static/media/verdeGun.5de7af00.jpg"
+  },
+  {
+    "revision": "475e46e122c12002c17d76dc841fea23",
+    "url": "/static/media/verdeInternal.475e46e1.png"
+  },
+  {
+    "revision": "95aa9a766e4dd30799fbacc800615262",
+    "url": "/static/media/verdeLips.95aa9a76.jpg"
+  },
+  {
+    "revision": "1c5edd93b80e248625b4e7b68f314408",
+    "url": "/static/media/verdeOccupy.1c5edd93.jpg"
+  },
+  {
+    "revision": "c0cc2fcfd5324f67b583cf488a4d9abe",
+    "url": "/static/media/verdeSam.c0cc2fcf.jpg"
+  },
+  {
+    "revision": "d9459459c304634c425aa681980bb43a",
+    "url": "/static/media/verdeSweetheart.d9459459.jpg"
+  },
+  {
+    "revision": "514a00be4f82ffc8e485781e9f0a3e32",
+    "url": "/static/media/whiteboard.514a00be.gif"
+  },
+  {
+    "revision": "568d6feca1921f80d77f850de596fac1",
+    "url": "/static/media/wonky1.568d6fec.jpg"
+  },
+  {
+    "revision": "4f79d06db06a1c975be462e9273e4679",
+    "url": "/static/media/wonky2.4f79d06d.jpg"
+  },
+  {
+    "revision": "d7b96df910db29a91c6babc79ca6d198",
+    "url": "/static/media/young1_v2.d7b96df9.jpg"
+  },
+  {
+    "revision": "185533d6e466a17ac08c77f8a54f6f0b",
+    "url": "/static/media/young2_s.185533d6.png"
+  },
+  {
+    "revision": "25f2a5f1d56c220fda988498bc78902e",
+    "url": "/static/media/young4_s.25f2a5f1.png"
+  },
+  {
+    "revision": "b5d3f49f53fb06bebe4640c5bff9b9f8",
+    "url": "/static/media/young5_s.b5d3f49f.png"
+  },
+  {
+    "revision": "55cfeb1278fa4d9588a81546efa6e80a",
+    "url": "/static/media/young7_s.55cfeb12.png"
+  },
+  {
+    "revision": "2d577553402de9222307b71fc822d806",
+    "url": "/static/media/young8_s.2d577553.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3fda34116108dab8b4533cf3e017f780.js"
+  "/precache-manifest.1a3399ce4ae527154500b42f506756dc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

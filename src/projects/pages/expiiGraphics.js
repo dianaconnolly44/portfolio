bbd77da8c9e94564e3eda5372395442ee50327e1
Diana Connolly <dianaconnolly44@gmail.com>
2020-07-22 17:51:52 -0400

    added links to graphics on expii graphics page, forgot to mention that last update also changed the colors and names of the medikidz

--- src/projects/pages/expiiGraphics.js
+++ src/projects/pages/expiiGraphics.js
@@ -9,55 +9,55 @@ export const page = (
 <div className="imgs max500">
             <div>
                 <img src={require('../assets/expiiGraphics/19.png')} />
-                <p>Winter Olympics</p>
+                <p>"Winter Olympics", click <a href="https://www.expii.com/problem/18624" target="_blank">here</a> to see corresponding math problem</p>
             </div>
             <div>
                 <img src={require('../assets/expiiGraphics/16.png')} />
-                <p>Oscars</p>
+                <p>"Oscars", click <a href="https://www.expii.com/t/interpreting-relations-and-the-vertical-line-test-4754?type=problem&id=18627&solution=9176" target="_blank">here</a> to see corresponding math problem</p>
             </div>
         </div>
 
 <div className="imgs max500">
             <div>
                 <img src={require('../assets/expiiGraphics/12.png')} />
-                <p>Groundhog Day</p>
+                <p>"Groundhog Day", click <a href="https://www.expii.com/t/combined-linear-systems-of-equations-and-inequalities-5037?type=problem&id=18473" target="_blank">here</a> to see corresponding math problem</p>
             </div>
             <div>
                 <img src={require('../assets/expiiGraphics/17.png')} />
-                <p>Presidents Day</p>
+                <p>"Presidents Day", click <a href="https://www.expii.com/t/intro-to-relations-4309?type=problem&id=19108&solution=9296" target="_blank">here</a> to see corresponding math problem</p>
             </div>
         </div>
         
 <div className="imgs max500">
             <div>
                 <img src={require('../assets/expiiGraphics/6.png')} />
-                <p>Chess Day</p>
+                <p>"Chess Day", click <a href="https://www.instagram.com/p/BWvGfOJDJiD/" target="_blank">here</a> to see corresponding instagram post</p>
             </div>
             <div>
                 <img src={require('../assets/expiiGraphics/7.png')} />
-                <p>Pie Approximation Day</p>
+                <p>"Pi Approximation Day", click <a href="https://www.instagram.com/p/BW3PPWFDoWC/" target="_blank">here</a> to see corresponding instagram post</p>
             </div>
         </div>  
 
 <div className="imgs max500">
             <div>
                 <img src={require('../assets/expiiGraphics/21.png')} />
-                <p>Waffle Day</p>
+                <p>"Waffle Day", click <a href="https://www.expii.com/t/applications-of-area-and-perimeter-5477?type=problem&id=19574" target="_blank">here</a> to see corresponding math problem</p>
             </div>
             <div>
                 <img src={require('../assets/expiiGraphics/8.png')} />
-                <p>Watermelon Day</p>
+                <p>"Watermelon Day", click <a href="https://www.instagram.com/p/BXYNJEEDx7Y/" target="_blank">here</a> to see corresponding instagram post</p>
             </div>
         </div>
 
 <div className="imgs max500">
             <div>
                 <img src={require('../assets/expiiGraphics/13.png')} />
-                <p>Chinese New Year</p>
+                <p>"Chinese New Year", click <a href="https://www.expii.com/t/logic-puzzles-and-brainteasers-10611?type=problem&id=18572" target="_blank">here</a> to see corresponding math problem</p>
             </div>
             <div>
                 <img src={require('../assets/expiiGraphics/10.png')} />
-                <p>Pluto Demoted Day</p>
+                <p>"Pluto Demoted Day", click <a href="https://www.instagram.com/p/BYMBpacDi7E/" target="_blank">here</a> to see corresponding instagram post</p>
             </div>
         </div>
 
@@ -66,22 +66,22 @@ export const page = (
 <div className="imgs max500">
             <div>
                 <img src={require('../assets/expiiGraphics/11.png')} />
-                <p>Total Eclipse</p>
+                <p>"Total Eclipse", click <a href="https://www.instagram.com/p/BYD1PZDjNux/" target="_blank">here</a> to see corresponding instagram post</p>
             </div>
             <div>
                 <img src={require('../assets/expiiGraphics/14.png')} />
-                <p>Kid Inventor Day (K.I.D.)</p>
+                <p>"Kid Inventor Day, K.I.D.", click <a href="https://www.expii.com/t/logic-puzzles-and-brainteasers-10611?type=problem&id=18634&solution=8146" target="_blank">here</a> to see corresponding math problem</p>
             </div>
         </div>
 
 <div className="imgs max500">
             <div>
                 <img src={require('../assets/expiiGraphics/9.png')} />
-                <p>Cat Day</p>
+                <p>"Cat Day", click <a href="https://www.instagram.com/p/BXijJHrjfnP/" target="_blank">here</a> to see corresponding instagram post</p>
             </div>
             <div>
                 <img src={require('../assets/expiiGraphics/2.png')} />
-                <p>Tau Day</p>
+                <p>"Tau Day", click <a href="https://www.instagram.com/p/BV5RrBhFTic/" target="_blank">here</a> to see corresponding instagram post</p>
             </div>            
         </div>
 

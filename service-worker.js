c902102f2479cfdcba0087c383d674c02a4dca71
Diana Connolly <dianaconnolly44@gmail.com>
2020-07-30 13:03:08 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4b192efdb9a948dd4dbdafac0a4e21b9.js"
+  "/precache-manifest.22510eb73094aed9a44552982cad7785.js"
 );
 
 self.addEventListener('message', (event) => {

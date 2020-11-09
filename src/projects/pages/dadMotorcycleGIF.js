2277a34240ca5697f03ec965cbfb89ea69b2de54
Diana Connolly <dianaconnolly44@gmail.com>
2020-11-09 10:10:10 -0500

    updated site with duolingo resume and fixed dad motorcycle page title

--- src/projects/pages/dadMotorcycleGIF.js
+++ src/projects/pages/dadMotorcycleGIF.js
@@ -4,7 +4,7 @@ export const Page = props => (
 
     {/* ==== EDIT BELOW THIS LINE ==== */}
     
-    <h1> "Anatomical Valentine": Animated Heart GIF </h1>
+    <h1> "Dad's Motorcycle" GIF </h1>
 
 <p> I created this GIF as a Father's Day gift to my dad, to celebrate
 how cool he is with his motorcycle riding around San Francisco. It was key
